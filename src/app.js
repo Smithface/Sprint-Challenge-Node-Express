856d4c1604ae8c6481ef0e467b77bd829f83ed6b
Smithface <kylewibblesmith@gmail.com>
2018-03-09 11:18:13 -0500

    finished app.js
    next to write the controler

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const checkValController = require('./controllers/checkVal.js');
+
+const app = express();
+const PORT = 3030;
+
+app.use(checkValController);
+
+app.listen(PORT, err => {
+  if (err) {
+    console.error('Error starting server:', err);
+  } else {
+    console.log('Server listening on port', PORT);
+  }
+})
